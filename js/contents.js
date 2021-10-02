d41bc269a373a39f8610d20a5dc4012d90fb74b3
delzhand <delzhand@gmail.com>
2021-10-01 22:04:02 -0400

    first-time message

--- js/contents.js
+++ js/contents.js
@@ -8,33 +8,33 @@ contents = [
 function drawNav() {
   $('.footer').append(`
   <div class="menu pages">
-    <a class="first-page" href="#"><< First</a>
-    <a class="prev-page" href="#">< Prev</a>
-    <a class="next-page" href="#">Next ></a>
-    <a class="latest-page" href="#">Latest >></a>
+    <a class="first-page" ><< First</a>
+    <a class="prev-page" >< Prev</a>
+    <a class="next-page" >Next ></a>
+    <a class="latest-page" >Latest >></a>
   </div>
   <div class="menu nav">
     <a href="index.html">Home</a>
-    <a href="#" disabled>Party Status</a>
+    <a  disabled>Party Status</a>
     <a href="about.html">About</a>
-    <a href="#" disabled>Archive</a>
-    <a href="#" disabled>Updates</a>
+    <a  disabled>Archive</a>
+    <a  disabled>Updates</a>
   </div>
 `);
 
 $('.header').append(`
   <div class="menu nav">
     <a href="index.html">Home</a>
-    <a href="#" disabled>Party Status</a>
+    <a  disabled>Party Status</a>
     <a href="about.html">About</a>
-    <a href="#" disabled>Archive</a>
-    <a href="#" disabled>Updates</a>
+    <a  disabled>Archive</a>
+    <a  disabled>Updates</a>
   </div>
   <div class="menu pages">
-    <a class="first-page" href="#"><< First</a>
-    <a class="prev-page" href="#">< Prev</a>
-    <a class="next-page" href="#">Next ></a>
-    <a class="latest-page" href="#">Latest >></a>
+    <a class="first-page" ><< First</a>
+    <a class="prev-page" >< Prev</a>
+    <a class="next-page" >Next ></a>
+    <a class="latest-page" >Latest >></a>
   </div>
 `);
 }
@@ -53,7 +53,7 @@ function populateLinks() {
   }
   else if (contentIndex === contents.length - 1 || currentPage === 'index.html') {
     $('a.first-page').attr('href', contents[0]);
-    $('a.prev-page').attr('href', contents[contentIndex-1]);
+    $('a.prev-page').attr('href', contents[contents.length-2]);
     $('a.next-page').attr('disabled', 'disabled');
     $('a.latest-page').attr('disabled', 'disabled');
   }
