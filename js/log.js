03e152a2cbd49906fb6118d1933a32ebbdcd38d0
delzhand <delzhand@gmail.com>
2021-10-11 09:45:04 -0400

    Update log.js

--- js/log.js
+++ js/log.js
@@ -8,6 +8,7 @@ log = [
   {type: 'story', date: '10/02/2021', txt: 'Saoirse awakens at home in the fishing village of Bolba - but what of her test flight on Branford?<br><a href="story.html?scene=4">Page 1 of 3</a>'},
   {type: 'system', date: '10/03/2021', txt: 'Party Status page unlocked!'},
   {type: 'story', date: '10/04/2021', txt: 'Saoirse begins thinking of a way to repair Branford and get airborne again.<br><a href="story.html?scene=6">Page 1 of 2</a>'},
+  {type: 'party', date: '10/07/2021', txt: `Saoirse gained 18 XP and leveled up! (+5 HP)`},
   {type: 'story', date: '10/07/2021', txt: 'Saoirse decides to help out a village farmer, even if it means tackling a small predator.<br><a href="story.html?scene=8">Page 1 of 3</a>'},
   {type: 'party', date: '10/09/2021', txt: `Saoirse learned [Arc-Flash]`},
   {type: 'story', date: '10/09/2021', txt: `Saoirse takes shelter in a cave, but there's something else in here...<br><a href="story.html?scene=10">Page 1 of 3</a>`},
